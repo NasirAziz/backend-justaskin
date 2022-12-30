a6fb272bed277ee090a59f45eb09fcf37ed03c20
Nasir Aziz <nasiraziz08@gmail.com~>
2022-12-30 17:00:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Pool, Client } = require("pg")
+require("dotenv").config()
+
+const pool = new Pool({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.POSTGRESQL_DB,
+  //   dialect: "postgres",
+  //   idleTimeoutMillis: 30000,
+  //   connectionTimeoutMillis: 2000,
+})
+
+// promise - checkout a client
+pool
+  .connect()
+  .then(async (client) => {
+    console.log("Connected", client)
+    // try {
+    //   const res = await client.query("SELECT * FROM users WHERE id = $1", [1])
+    //   client.release()
+    //   console.log(res.rows[0])
+    // } catch (err_1) {
+    //   client.release()
+    //   console.log(err_1.stack)
+    // }
+
+    // remove when necessary
+    pool.end()
+  })
+  .catch((err) => {
+    console.log("Error Connecting with server:", JSON.stringify(err))
+  })
