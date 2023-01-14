881fec14081cda34c36902f6b1cfb4f299ad9a60
Muhammad Nasir Aziz <nasiraziz08@gmail.com>
2023-01-14 10:05:40 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const { Pool, Client } = require("pg")
-require("dotenv").config()
+const admin = require("./connection/firebase");
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+const blogRoutes = require("./routes/blogPosts.jsx");
 
-const pool = new Pool({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
-  //   dialect: "postgres",
-  // idleTimeoutMillis: 30000,
-  // connectionTimeoutMillis: 2000,
-})
-
-// promise - checkout a client
-pool
-  .connect()
-  .then(async (client) => {
-    console.log("Connected", client)
-    try {
-      const res = await client.query("SELECT * FROM users WHERE user_id = $1", [1])
-      client.release()
-      console.log(res.rows[0])
-    } catch (err_1) {
-      client.release()
-      console.log(err_1.stack)
-    }
-
-    // remove when necessary
-    pool.end()
-  })
-  .catch((err) => {
-    console.log("Error Connecting with server:", JSON.stringify(err))
-  })
+const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+const app = express();
+app.use(cors(corsOptions));
+app.use("/users", require("./routes/users"));
+app.use("/api/blogPosts", blogRoutes);
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
