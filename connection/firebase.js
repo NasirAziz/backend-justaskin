fb98128f42ab39025544d31ebfa5013ec131f4e5
Muhammad Nasir Aziz <nasiraziz08@gmail.com>
2023-01-15 17:57:53 +0500

    Add files via upload

--- connection/firebase.js
+++ connection/firebase.js
@@ -1,14 +1,24 @@
-const admin = require("firebase-admin");
 require("dotenv").config();
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MSG_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASURE_ID,
-};
-admin.initializeApp(firebaseConfig);
 
-module.exports = admin;
+const config = process.env.SERVICE_CONFIG;
+
+const firebaseAdminSdk = require("firebase-admin"),
+  firebaseAdminApp = firebaseAdminSdk.initializeApp({
+    credential: firebaseAdminSdk.credential.cert(
+      JSON.parse(
+        Buffer.from(process.env.SERVICE_CONFIG, "base64").toString("ascii")
+      )
+    ),
+  });
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MSG_ID,
+//   appId: process.env.APP_ID,
+//   measurementId: process.env.MEASURE_ID,
+// };
+// admin.initializeApp(firebaseConfig);
+
+module.exports = firebaseAdminApp;
