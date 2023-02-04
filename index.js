051f96f6ed50d057fefbc836ca4dc3d4d8ff825a
haseeb4091 <74922363+haseeb4091@users.noreply.github.com>
2023-02-04 19:07:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require("./connection/firebase")
+require("./connection/firebase");
 const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
 const blogRoutes = require("./routes/blogPosts.jsx");
+const emailRoute = require("./routes/emailManagment");
 
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
@@ -12,8 +13,9 @@ const corsOptions = {
 };
 const app = express();
 app.use(cors(corsOptions));
-app.use("/users", require("./routes/users"));
+app.use("/api/users", require("./routes/users"));
 app.use("/api/blogPosts", blogRoutes);
-app.listen(PORT, () => {
+app.use("/api/email", emailRoute);
+app.listen(5000, () => {
   console.log("Example app listening on port " + PORT);
 });
