0abf5b1767b805ab199132d7f7c15ef22ebc8efb
haseeb4091 <74922363+haseeb4091@users.noreply.github.com>
2023-02-05 12:56:44 +0500

    Update emailManagment.js

--- routes/emailManagment.js
+++ routes/emailManagment.js
@@ -11,16 +11,26 @@ const sendEmail = (email) => {
     var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "umerk7222@gmail.com",
+        user: process.env.EMAIL,
         pass: process.env.PASSWORD,
       },
     });
 
     var mailOptions = {
       from: "umerk7222@gmail.com",
-      to: "mail@jusaskin.com",
-      subject: "JUSASKIN APP NEWS LETTER",
-      text: email + "" + " has subscribed",
+      to: email,
+      subject: "Subject: Welcome to Jus Askin!",
+      text: `   
+      Dear,
+      
+      We are thrilled to have you on board as a new member of Jus Askin! We are excited to share our platform with you and help you make the most out of it.
+      At Jus Askin, we are dedicated to providing you with an easy and convenient way to ask and receive answers to your questions with valuable content. Our community of knowledgeable individuals is always here to help, and we believe that you will find the information you need in no time.
+      
+      Thank you for signing up and we look forward to your participation in our community. 
+      If you have any questions or feedback, please don't hesitate to reach out to us. We are always here to help.
+      
+      Best regards,
+      The Jus Askin Team`,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
