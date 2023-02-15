b6b83ccb77a0cbb512302d4c09cff255b9196faf
haseeb4091 <74922363+haseeb4091@users.noreply.github.com>
2023-02-15 15:42:40 +0500

    html email

--- routes/emailManagment.js
+++ routes/emailManagment.js
@@ -27,18 +27,7 @@ const sendEmail = (email) => {
       from: "umerk7222@gmail.com",
       to: email,
       subject: "Subject: Welcome to Jus Askin!",
-      text: `   
-      Dear,
-      
-      We are thrilled to have you on board as a new member of Jus Askin! We are excited to share our platform with you and help you make the most out of it.
-      At Jus Askin, we are dedicated to providing you with an easy and convenient way to ask and receive answers to your questions with valuable content. 
-      Our community of knowledgeable individuals is always here to help, and we believe that you will find the information you need in no time.
-      
-      Thank you for signing up and we look forward to your participation in our community. 
-      If you have any questions or feedback, please don't hesitate to reach out to us. We are always here to help.
-      
-      Best regards,
-      The Jus Askin Team`,
+      html: `<!DOCTYPE html> <html> <head> <title>Welcome to Jus Askin</title> </head> <body> <h1>Hello!</h1> <p>Thank you for subscribing to Jus Askin! We are thrilled to have you on board and can't wait to share our platform with you.</p> <p>As we approach our upcoming launch, we wanted to give you a sneak peek into some of the exciting features we have in store for you. With Jus Askin, you will have access to a community of knowledgeable individuals who are always ready to help you with valuable content. You can ask questions and receive answers quickly and easily, no matter where you are.</p> <p>Our platform is designed to make it easy and convenient for you to get the information you need. With Jus Askin, you can search for questions on a wide range of topics, connect with other users who share your interests, and gain insights from experts in your field.</p> <p>We are excited to have you join our community and look forward to your participation. If you have any questions or feedback, please don't hesitate to reach out to us. We are always here to help.</p> <p>Thank you again for subscribing to Jus Askin. We can't wait to see you on the platform soon!</p> <p>Best regards,</p> <p>The Jus Askin Team</p> </body> </html>`,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
