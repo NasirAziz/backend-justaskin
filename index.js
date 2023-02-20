4ecab3f423215aebc2876dd181bb8e1cae3ff232
haseeb4091 <74922363+haseeb4091@users.noreply.github.com>
2023-02-20 13:28:53 +0500

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 require("dotenv").config();
 const blogRoutes = require("./routes/blogPosts.jsx");
 const emailRoute = require("./routes/emailManagment");
-
+const admin = require('./routes/Admin')
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: "*",
@@ -16,6 +16,8 @@ app.use(cors(corsOptions));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/blogPosts", blogRoutes);
 app.use("/api/email", emailRoute);
+app.use("/api/admin", admin);
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
